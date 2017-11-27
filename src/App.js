c36ca2c526d556df7f1b267c6efc25a82293a0bb
Samn2npa <2npa2812@gmail.com>
2017-11-27 13:05:43 +0700

    update chunk

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import React, { Component } from 'react';
-import logo from './flixie_logo.png';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./flixie_logo.png";
+import "./App.css";
 import { Container } from "bloomer";
 import "bulma/css/bulma.css";
 import MovieList from "./MovieList";
+import MovieCard from "./MovieCard";
+import { Column } from "bloomer/lib/grid/Column";
+import { Columns } from "bloomer/lib/grid/Columns";
 
 class App extends Component {
   constructor(props) {
@@ -12,15 +15,17 @@ class App extends Component {
       movies: [],
       isLoading: true,
       page: 1
-    }
+    };
   }
 
   sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms))
+    return new Promise(resolve => setTimeout(resolve, ms));
   }
 
   async fetchMovies(page) {
-    const url = 'https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed&page='+ page;
+    const url =
+      "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed&page=" +
+      page;
     console.log("Samn: " + url);
 
     const result = await fetch(url);
@@ -33,26 +38,41 @@ class App extends Component {
     this.setState({
       movies: this.movies,
       isLoading: false
-    })
+    });
   }
 
   async onLoadMore() {
     const numpage = this.state.page + 1;
-    const newMovies = await this.fetchMovies(numpage); 
+    const newMovies = await this.fetchMovies(numpage);
     this.movieResult = this.state.movies.concat(newMovies);
     this.setState({
       page: numpage,
       movies: this.movieResult
-    })
+    });
+  }
+
+  chunk(array, size) {
+    return array.reduce((chunks, item, i) => {
+      if (i % size === 0) {
+        chunks.push([item]);
+      } else {
+        chunks[chunks.length - 1].push(item);
+      }
+      return chunks;
+    }, []);
   }
 
   render() {
     let content;
     if (this.state.isLoading) {
-      content = <h1>LOADING </h1>
+      content = <h1>LOADING </h1>;
     } else {
-      content = <MovieList onClickLoadMore={(e) => this.onLoadMore(e)}
-        movies={this.state.movies} />
+      content = (
+        <MovieList
+          onClickLoadMore={e => this.onLoadMore(e)}
+          movies={this.state.movies}
+        />
+      );
     }
 
     return (
@@ -63,15 +83,19 @@ class App extends Component {
             <h1 className="App-title">Welcome to Flixie</h1>
           </header>
 
-          <br/>
-
-          
-          
-          
           <Container>
-            {content}
-          </Container>
+            {/* {content} */
 
+            this.chunk(this.state.movies, 3).map((c, idx) => (
+              <Columns isCentered>
+                {c.map(m => (
+                  <Column isSize="1/3">
+                    <MovieCard movie={m} key={m.id} />
+                  </Column>
+                ))}
+              </Columns>
+            ))}
+          </Container>
         </div>
       </Container>
     );
--- src/MovieCard.jsx
+++ src/MovieCard.jsx
@@ -9,15 +9,32 @@ import {
   CardTitle,
   CardSubtitle,
   Button,
-  CardGroup
+  CardGroup,
+  Collapse
 } from "reactstrap";
 import { Columns } from "bloomer/lib/grid/Columns";
 import { Column } from "bloomer/lib/grid/Column";
 
 export default class MovieCard extends Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      isCollapsed: false 
+    }
+  }
+
   getSrcPoster(poster_path) {
     const defaultPath = "https://image.tmdb.org/t/p/original";
     return defaultPath + poster_path;
+  }
+
+  clickMore(){
+    this.collaped = this.state.isCollapsed;
+    this.collaped == false ? true : false;
+    this.setState({
+      isCollapsed: this.collaped
+    });
   }
 
   render() {
@@ -35,8 +52,12 @@ export default class MovieCard extends Component {
             src={this.getSrcPoster(movie.poster_path)}
           />
           <CardBody>
-            <CardTitle>{movie.title}</CardTitle>
+          <CardTitle>{movie.title}</CardTitle>
+            <Button color="primary" onClick= {this.clickMore}>More</Button>
+            <Collapse isOpen={this.state.isCollapsed}>
+            
             <CardText>{movie.overview}</CardText>
+            </Collapse>            
           </CardBody>
         </Card>
     
